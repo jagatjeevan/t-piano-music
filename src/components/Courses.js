51455d4b6162fc1a882b6ebfd1ce7f4e73e54480
Jagat Jeevan Sahoo <kanhajeevan@gmail.com>
2023-12-29 10:37:58 +0530

    partnered

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -8,7 +8,7 @@ const courses = [
     alt: 'Learn Guitar',
     name: 'Guitar',
     description:
-      'Learn how to play Guitar from the master from beginner to advance level. The lessons are so well taught, the school has really changed the lives of millions of people all over the world, keep the good songs coming!',
+      'Learn how to play Guitar from the master from beginner to advance level. The lessons are so well taught, the school has really changed the lives of millions of people all over the world, keep the good songs coming! Bassists are essential to holding the groove of a song together. Learn the techniques of many great bassist with Rockschool Bass Grades. We’ve hand-picked tunes from some of the world’s biggest artists across a diverse range of genres to provide students with an expansive song choice to use in their exam and for their own enjoyment. Learn your favourite styles of music with Rockschool Electric Guitar.',
   },
   {
     id: 2,
@@ -16,7 +16,7 @@ const courses = [
     alt: 'Piano',
     name: 'Piano class',
     description:
-      'The school has changed the mindset of millions. The institute has been very good in communicating and imparting knowledge to make great musician. Practice notes and chords interactively and receive instant feedback. All you need is the flowkey app and your piano or keyboard.',
+      "If you're looking to become a remarkable keyboardist, the Rockschool Keys syllabus will support you on your musical journey today. The school has changed the mindset of millions. The institute has been very good in communicating and imparting knowledge to make great musician. Practice notes and chords interactively and receive instant feedback. All you need is the flowkey app and your piano or keyboard. Created with the modern musician in mind, Rockschool Piano grades gives all aspiring pianists the knowledge needed to become a dynamic contemporary performer.",
   },
   {
     id: 3,
@@ -24,7 +24,7 @@ const courses = [
     alt: 'Learn Drums',
     name: 'Drums',
     description:
-      'The drums lie at the core of understanding music – beats and rhythms. We offer personalised training to identify your style as a drummer – from the basics to more advanced routines and grooves. Jazz, rock, pop, or metal, our classes will help you become a passionate drummer in the genre of your choice! We also expose our students to performance-based drumming and jam live with other instrumentalists and vocalists.',
+      'The drummer is the driving force, backbone and foundation of the entire band dynamic. Learn the fundamentals to becoming an awesome drummer with Rockschool Drum grades. The drums lie at the core of understanding music – beats and rhythms. We offer personalised training to identify your style as a drummer – from the basics to more advanced routines and grooves. Jazz, rock, pop, or metal, our classes will help you become a passionate drummer in the genre of your choice! We also expose our students to performance-based drumming and jam live with other instrumentalists and vocalists.',
   },
 ];
 
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,35 +1,55 @@
+import Image from 'next/image';
 import styles from '../styles/Welcome.module.scss';
 
 const Welcome = () => {
   return (
-    <div className="box">
-      <h1 className="heading">Welcome</h1>
-      <article className={styles.description}>
+    <>
+      <div className="box">
+        <h1 className="heading">Welcome</h1>
+        <article className={styles.description}>
+          <p>
+            Welcome to T&apos; Music School! We offer music education for individuals of just about
+            every age and skill level. We believe music education is not only about acquiring
+            knowledge, it&apos;s about igniting a lifelong passion and fostering creativity. With a
+            deep commitment to quality education and a holistic approach to learning, we have
+            established ourselves as a premier destination for music education.
+          </p>
+          <p>
+            Unleash Your Musical Talent with Expert Guidance in Drums, Guitar, Piano, and More!
+            Explore several art forms as your child builds creativity and confidence in our new
+            visual arts classes for ages 2-5.
+          </p>
+          <p>
+            Dive into various genres and styles, from classical to contemporary, Indian music to
+            Western beats. Learn from the finest musicians with years of performing and teaching
+            experience.
+          </p>
+          <p>
+            Choose from in-person or online classes to fit your schedule. Practice in fully equipped
+            music studios and rehearsal spaces. Access to advanced degree programs in future. You
+            can improve your musical skills like our students below by enrolling in our specialized
+            classes.
+          </p>
+        </article>
+      </div>
+      <div className="box">
+        <h1 className="heading">Partnered with</h1>
         <p>
-          Welcome to T&apos; Music School! We offer music education for individuals of just about
-          every age and skill level. We believe music education is not only about acquiring
-          knowledge, it&apos;s about igniting a lifelong passion and fostering creativity. With a
-          deep commitment to quality education and a holistic approach to learning, we have
-          established ourselves as a premier destination for music education.
+          It is affiliated to{' '}
+          <strong>
+            <a href="https://www.rslawards.com/rockschool/" target="_blank">
+              RSL
+            </a>
+          </strong>{' '}
+          music academy. You would get a certification of the school.
         </p>
-        <p>
-          Unleash Your Musical Talent with Expert Guidance in Drums, Guitar, Piano, and More!
-          Explore several art forms as your child builds creativity and confidence in our new visual
-          arts classes for ages 2-5.
-        </p>
-        <p>
-          Dive into various genres and styles, from classical to contemporary, Indian music to
-          Western beats. Learn from the finest musicians with years of performing and teaching
-          experience.
-        </p>
-        <p>
-          Choose from in-person or online classes to fit your schedule. Practice in fully equipped
-          music studios and rehearsal spaces. Access to advanced degree programs in future. You can
-          improve your musical skills like our students below by enrolling in our specialized
-          classes.
-        </p>
-      </article>
-    </div>
+        <div className={styles.affiliate}>
+          <a href="https://www.rslawards.com/rockschool/" target="_blank">
+            <Image src="/rsl.svg" width="100" height="100" alt="RSL school" />
+          </a>
+        </div>
+      </div>
+    </>
   );
 };
 
